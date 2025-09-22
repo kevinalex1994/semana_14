99238b16cd61a39414caa946bba0968a56d67692
kevinalex1994 <159743155+kevinalex1994@users.noreply.github.com>
2025-09-21 23:21:32 -0500

    Update activate.bat

--- activate.bat
+++ activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\PERSONAL\Pictures\Almacenes Sony\venv"
+set "VIRTUAL_ENV=C:\Users\PERSONAL\Pictures\Almacenes Marcimex\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -32,3 +32,4 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
     set _OLD_CODEPAGE=
 )
+
